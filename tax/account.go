53b8f8cba4eacad3c6b701bc32a9e6a19346ae96
nomadic4life <orbison.dennis@gmail.com>
2021-07-06 20:38:08 -0700

    implementing methods for Account Struct

--- tax/account.go
+++ tax/account.go
@@ -1,5 +1,7 @@
 package tax
 
+import "fmt"
+
 func NewAccount(capital float64) *Account {
 	account := Account{}
 	account.Statement.TotalCapital = capital
@@ -11,4 +13,37 @@ func NewAccount(capital float64) *Account {
 	account.AssetsHoldings["DOGE"] = 0.0
 	account.AssetsHoldings["XRP"] = 0.0
 	return &account
+}
+
+func (a *Account) CreateTrade(t Trade) {
+	fmt.Println(t)
+}
+
+func (a *Account) init(t Transaction) { // -> transaction
+	fmt.Println(t)
+}
+
+func (a *Account) outFlow(t Transaction) { // -> trade, transaction
+	fmt.Println(t)
+}
+
+func (a *Account) inflow(t Transaction) { // -> trade, transaction
+	fmt.Println(t)
+}
+
+func (a *Account) deduct(t Transaction) { // -> trade
+	fmt.Println(t)
+}
+
+func (a *Account) append(t Transaction) { // -> queue, trade, transaction
+	fmt.Println(t)
+}
+
+func (a *Account) updateAccount(t Transaction) { // -> trade, transaction
+	fmt.Println(t)
+}
+
+func (a *Account) getID() int64 {
+	fmt.Println(len(a.Ledger.TransactionHistory))
+	return int64(len(a.Ledger.TransactionHistory))
 }
